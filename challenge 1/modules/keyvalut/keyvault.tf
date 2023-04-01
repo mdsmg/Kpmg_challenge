09c4ea4e07dc37d24ee91bea01081e5357ca7ec8
unknown <shafimdsmg@gmail.com>
2023-04-01 23:10:43 +0530

    save

--- challenge 1/modules/keyvalut/keyvault.tf
+++ challenge 1/modules/keyvalut/keyvault.tf
@@ -1,8 +1,8 @@
 
 resource "azurerm_key_vault" "example" {
   name                = "my-key-vault"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = data.azurerm_storage_group.azure-rg.name
+  resource_group_name = data.azurerm_storage_container.azure-rg.name
   enabled_for_deployment = true
   enabled_for_disk_encryption = true
   tenant_id           = data.azurerm_client_config.current.tenant_id
@@ -76,25 +76,3 @@ data "azurerm_key_vault_secret" "db_password" {
   key_vault_id = azurerm_key_vault.example.id
 }
 
-/*output "web_username" {
-  value = data.azurerm_key_vault_secret.web_username.value
-}
-
-output "web_password" {
-  value = data.azurerm_key_vault_secret.web_password.value
-}
-
-output "app_username" {
-  value = data.azurerm_key_vault_secret.app_username.value
-}
-
-output "app_password" {
-  value = data.azurerm_key_vault_secret.app_password.value
-}
-output "db_username" {
-  value = data.azurerm_key_vault_secret.db_username.value
-}
-
-output "db_password" {
-  value = data.azurerm_key_vault_secret.db_password.value
-}*/
--- challenge 1/modules/resourcegroup/main.tf
+++ challenge 1/modules/resourcegroup/main.tf
@@ -2,3 +2,18 @@ resource "azurerm_resource_group" "azure-rg" {
   name     = var.name
   location = var.location
 }
+
+data "azurerm_resource_group" "azure-rg" {
+  name                = azurerm_resource_group.azure-rg.name
+
+}
+
+data "azurerm_storage_container" "azure-rg" {
+  name                = azurerm_storage_container.azure-rg.name
+  
+}
+
+data "azurerm_storage_location" "azure-rg" {
+  name                = azurerm_storage_location.azure-rg.name
+  
+}
