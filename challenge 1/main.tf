9465bd63577e091664880b09e3287cacbbb4d939
mdsmg <129238320+mdsmg@users.noreply.github.com>
2023-04-01 17:15:13 +0530

    Update main.tf

--- challenge 1/main.tf
+++ challenge 1/main.tf
@@ -1,8 +1,6 @@
 provider "azurerm" {
   features {}
 }
-
-
 terraform {
   required_providers {
     azurerm = {
@@ -11,26 +9,22 @@ terraform {
     }
   }
 }
-
 module "resourcegroup" {
   source         = "./modules/resourcegroup"
   name           = var.name
   location       = var.location
 }
-
 module "storageaccount" {
   source         = "./modules/storageaccount"
   resource_group = module.resourcegroup.resource_group_name
   location       = module.resourcegroup.location_id
 }
-
 module "backend" {
     source         = "./modules/backend"
     resource_group_name   = module.resourcegroup.resource_group_name
     storage_account_name  = module.azurerm_storage_account.storage.name
     container_name        = module.azurerm_storage_account.container.name
 }
-
 module "networking" {
   source         = "./modules/networking"
   location       = module.resourcegroup.location_id
@@ -40,7 +34,6 @@ module "networking" {
   appsubnetcidr  = var.appsubnetcidr
   dbsubnetcidr   = var.dbsubnetcidr
 }
-
 module "securitygroup" {
   source         = "./modules/securitygroup"
   location       = module.resourcegroup.location_id
@@ -49,7 +42,6 @@ module "securitygroup" {
   app_subnet_id  = module.networking.appsubnet_id
   db_subnet_id   = module.networking.dbsubnet_id
 }
-
 module "compute" {
   source         = "./modules/compute"
   location = module.resourcegroup.location_id
@@ -63,7 +55,6 @@ module "compute" {
   app_username = var.app_username
   app_os_password = var.app_os_password
 }
-
 module "database" {
   source = "./modules/database"
   location = module.resourcegroup.location_id
