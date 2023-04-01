6485d1aea8f62e3494bd2306b9e42a1ba1b5b30d
unknown <shafimdsmg@gmail.com>
2023-04-01 23:43:40 +0530

    save

--- challenge 1/providers.tf
+++ challenge 1/providers.tf
@@ -1,5 +1,11 @@
 provider "azurerm" {
   features {}
+
+  subscription_id = "your_subscription_id"
+  client_id       = "your_client_id"
+  client_secret   = "your_client_secret"
+  tenant_id       = "your_tenant_id"
+
 }
 terraform {
   required_providers {
