cd2e8c88b862149e2e50838d30c9dd485a7af089
mdsmg <129238320+mdsmg@users.noreply.github.com>
2023-04-02 11:16:37 +0530

    Update backend.tf

--- challenge 1/backend.tf
+++ challenge 1/backend.tf
@@ -1,4 +1,8 @@
-resource_group_name   = var.name
-storage_account_name  = data.azurerm_storage_account.terraform_storage.name
-container_name        = data.azurerm_storage_container.terraform_storage.name
-key  = "terraform.tfstate"
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "terraform-rg"
+    storage_account_name  = "terraformrg"
+    container_name        = "tfstate"
+    key                   = "terraform.tfstate"
+  }
+}
