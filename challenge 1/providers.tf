e539ca1705eed2213fbd6243730ed2c680385faf
mdsmg <129238320+mdsmg@users.noreply.github.com>
2023-04-02 15:11:28 +0530

    Update providers.tf

--- challenge 1/providers.tf
+++ challenge 1/providers.tf
@@ -1,10 +1,10 @@
 provider "azurerm" {
   features {}
 
-  subscription_id = "your_subscription_id"
-  client_id       = "your_client_id"
-  client_secret   = "your_client_secret"
-  tenant_id       = "your_tenant_id"
+  #subscription_id = "subscription_id"
+  #client_id       = "client_id"
+  #client_secret   = "client_secret"
+  #tenant_id       = "tenant_id"
 
 }
 terraform {
