1b06e538481c19cd5f1341b2913620c9ebb7851d
mdsmg <129238320+mdsmg@users.noreply.github.com>
2023-04-02 11:51:29 +0530

    Update main.tf

--- challenge 1/modules/storageaccount/main.tf
+++ challenge 1/modules/storageaccount/main.tf
@@ -1,33 +1,28 @@
 
-resource "azurerm_storage_account" "terraform_storage" {
+
+# Create Resource Group
+resource "azurerm_resource_group" "storage_rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# Create Storage Account
+resource "azurerm_storage_account" "terraform_backend" {
   name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.terraform_storage.name
-  location                 = azurerm_resource_group.terraform_storage.location
+  resource_group_name      = azurerm_resource_group.storage_rg.name
+  location                 = azurerm_resource_group.storage_rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
   tags = {
-    environment = "dev"
+    Environment = "dev"
   }
 }
 
-resource "azurerm_storage_container" "terraform_storage" {
-  name                  = var.storage_container_name
-  storage_account_name  = azurerm_storage_account.terraform_storage.name
+# Create Blob Container
+resource "azurerm_storage_container" "terraform_backend_container" {
+  name                  = var.container_name
+  storage_account_name  = azurerm_storage_account.terraform_backend.name
   container_access_type = "private"
 }
 
-data "azurerm_storage_account" "terraform_storage" {
-  name                = azurerm_storage_container.terraform_storage.name
-  resource_group_name = azurerm_resource_group.terraform_storage.name
-}
-
-data "azurerm_storage_container" "terraform_storage" {
-  name                = azurerm_storage_account.terraform_storage.name
-  resource_group_name = azurerm_resource_group.terraform_storage.name
-}
-
-data "azurerm_storage_account_key" "terraform_storage" {
-  storage_account_id = data.azurerm_storage_account.terraform_storage.id
-  key_type           = "primary"
-}
